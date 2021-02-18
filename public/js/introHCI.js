f5f7c23001cdc970483c8f763572faeeb1747acd
kelly-lo <lo.kelly626@gmail.com>
2021-02-17 21:48:25 -0800

    Added /viewAlt route and like button

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,11 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(sendAnalytics);
+}
+
+function sendAnalytics(e) {
+	e.preventDefault();
+
+	ga("send", "event", "like", "click");
 }
